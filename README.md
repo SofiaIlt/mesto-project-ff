dc3f089e2ff41362bc45ab6ceb3f094eea299d73
Sofia Iltner <iliasakurai@yandex.ru>
2024-09-19 15:39:44 +0300

    v1 check

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Проектная работа Mesto
 
+https://github.com/SofiaIlt/mesto-project-ff.git - ссылка на репозиторий github
+
+ - ссылка на gh pages
+
--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,33 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+const content = document.querySelector('.content');
+const placesList = content.querySelector('.places__list');
+
+function addCard(nameValue, linkValue) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  
+    cardElement.querySelector('.card__image').src = linkValue;
+    cardElement.querySelector('.card__image').alt = nameValue;
+    cardElement.querySelector('.card__title').textContent = nameValue;
+  
+    cardElement.querySelector('.card__like-button').addEventListener('click', function (evt) {
+      evt.target.classList.toggle('card__like-button_is-active');
+    });
+
+    cardElement.querySelector('.card__delete-button').addEventListener('click', function (evt) {
+        deleteCard(evt);
+    });
+  
+    placesList.append(cardElement);
+}
+
+function deleteCard(evt) {
+    evt.target.closest('.card').remove();
+}
+
+initialCards.forEach( (element) => {
+    addCard(element.name, element.link);
+});
